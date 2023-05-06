f7461e28a79c007df913cb53765bf86fb5d1deee
Cody <103610020@student.swin.edu.au>
2023-05-07 02:50:49 +1000

    Created/ Reworked UI, Now displays ammo count and relolad time. Includes the velocity information from previous UI but implemetended differently. Slighty reworked the reload mechanism to allow for tracking of reload time.

--- Ballistite Project/Assets/Scripts/Player/BespokePlayerController.cs
+++ Ballistite Project/Assets/Scripts/Player/BespokePlayerController.cs
@@ -11,6 +11,13 @@ namespace Platformer.Mechanics
     /// </summary>
     public class BespokePlayerController : MonoBehaviour
     {
+        //UI object and script, probably easier than linking every text field in the UI
+        //Being set automatically based on name in Awake(), could be set to public if this needs to be changed
+        private GameObject UIObject;
+        private uiController UIScript;
+        //part of new reload function, this is the value that changes as the reload time progresses, old reloadTime is used as a target value.
+        private float reloadTimeActive;
+        
         public AudioClip gunAudio;
         public AudioClip reloadAudio;
         public AudioClip landingAudio;
@@ -18,9 +25,12 @@ namespace Platformer.Mechanics
         public Transform barrel;
         public Transform muzzle;
         public GameObject projectile;
+        //updated UI so these aren't needed for now
+        /* 
         public TMP_Text velocity;
         public TMP_Text Hvelocity;
         public TMP_Text Vvelocity;
+        */
         public float shotForce = 1f;
         public float shotRecoil = 2f;
         public float reloadTime = 1f;
@@ -47,6 +57,13 @@ namespace Platformer.Mechanics
             shotCount = shotNumber;
             spawn = transform.position;
             soundMachine = GetComponent<AudioSource>();
+
+            UIObject = GameObject.Find("UI");
+            if (UIObject != null)
+            {
+                UIScript = UIObject.GetComponent<uiController>();
+            }
+            reloadTimeActive = reloadTime;
         }
 
         private bool debug = false;
@@ -58,6 +75,8 @@ namespace Platformer.Mechanics
 
         void Update()
         {
+            //I've updated the method used to change UI values so this isn't needed, I'll if needed we can change back
+            /*
             if (Input.GetButtonDown("Enable Debug Button 1"))
             {
                 ToggleDebug();
@@ -77,13 +96,13 @@ namespace Platformer.Mechanics
                 Hvelocity.gameObject.SetActive(false);
                 Vvelocity.gameObject.SetActive(false);
             }
-           
+           */
             if (grounded)
             {
                 if (shotCount == 0 && !reloading)
                 {
                     reloading = true;
-                    StartCoroutine(GunReload());
+                    StartCoroutine(GunReloadV2());
                 }
             }
             if (controlEnabled)
@@ -115,6 +134,7 @@ namespace Platformer.Mechanics
                     transform.position = spawn;
                 }
             }
+            UpdateUIValues();
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
@@ -132,6 +152,8 @@ namespace Platformer.Mechanics
                 grounded = false;
             }
         }
+        //The reload function has been updated so that reload can be displayed in the UI
+        /*
         IEnumerator GunReload()
         {
             
@@ -156,6 +178,32 @@ namespace Platformer.Mechanics
                 //Debug.LogError("CLICK");
             }
         }
+        */
+        IEnumerator GunReloadV2()
+        {
+            for (reloadTimeActive = 1f; reloadTimeActive > 0; reloadTimeActive -= Time.deltaTime)
+                yield return null;
+            shotCount++;
+            reloadTimeActive = reloadTime;
+
+            if (shotCount < shotNumber && grounded)
+            {
+                soundMachine.PlayOneShot(reloadAudio);
+                StartCoroutine(GunReloadV2());
+            }
+            else if (shotCount < shotNumber)
+            {
+                yield return new WaitUntil(() => grounded);
+                soundMachine.PlayOneShot(reloadAudio);
+                StartCoroutine(GunReloadV2());
+            }
+            if (shotCount == shotNumber)
+            {
+                soundMachine.PlayOneShot(reloadAudio);
+                reloading = false;
+                //Debug.LogError("CLICK");
+            }
+        }
 
         IEnumerator Cooldown()
         {
@@ -164,6 +212,16 @@ namespace Platformer.Mechanics
             yield return new WaitForSeconds(0.5f);
             cooldown = false;
             //Debug.Log("End Cooldown");
+        }
+
+        //calls functions from the UI's script to update values. Is called in update
+        private void UpdateUIValues()
+        {
+            if (UIScript != null)
+            {
+                UIScript.updateAmmoValues(shotCount, reloadTimeActive);
+                UIScript.updateVelocityValues(this.GetComponent<Rigidbody2D>().velocity.magnitude, this.GetComponent<Rigidbody2D>().velocity.x, this.GetComponent<Rigidbody2D>().velocity.y);
+            }
         }
     }
 }
---
+++ Ballistite Project/Assets/Scripts/UiController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using TMPro;
+
+public class uiController : MonoBehaviour
+{
+    private bool visisble;
+    public GameObject hideToggle;
+
+    public TMP_Text ammoCountText;
+    public TMP_Text reloadTimeText;
+    
+    private int ammoCountInt;
+    private float reloadTimeFloat;
+
+    public TMP_Text velocityText;
+    public TMP_Text hVelocityText;
+    public TMP_Text vVelocityText;
+
+    private float velocityFloat;
+    private float hVelocityFloat;
+    private float vVelocityFloat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visisble = false;
+        if (hideToggle != null)
+        {
+            hideToggle.SetActive(visisble);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (hideToggle != null && Input.GetButtonDown("Enable Debug Button 1"))
+        {
+            visisble = !visisble;
+            hideToggle.SetActive(visisble);
+        }
+
+        
+        setText();
+        
+    }
+
+    private void setText()
+    {
+        ammoCountText.text = "Ammo: " + ammoCountInt.ToString();
+        reloadTimeText.text = "Reloading: " + reloadTimeFloat.ToString();
+
+        velocityText.text = "velo: " + velocityFloat.ToString();
+        hVelocityText.text = "h velo: " + hVelocityFloat.ToString();
+        vVelocityText.text = "v velo: " + vVelocityFloat.ToString();
+
+    }
+
+    public void updateAmmoValues(int ammoC, float realoadT)
+    {
+        ammoCountInt = ammoC;
+        reloadTimeFloat = realoadT;
+    }
+
+    public void updateVelocityValues(float v, float hV, float vV)
+    {
+        velocityFloat = v;
+        hVelocityFloat = hV;
+        vVelocityFloat = vV;
+    }
+}
