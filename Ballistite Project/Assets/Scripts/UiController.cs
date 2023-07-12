e6dd99fbd4f816293ad352f338f3a334594688f4
Cody <103610020@student.swin.edu.au>
2023-07-13 03:55:43 +1000

    reverted some merges

--- Ballistite Project/Assets/Scripts/UiController.cs
+++ Ballistite Project/Assets/Scripts/UiController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class uiController : MonoBehaviour
 {
@@ -15,45 +16,81 @@ public class uiController : MonoBehaviour
     public TMP_Text velocityText;
     public TMP_Text hVelocityText;
     public TMP_Text vVelocityText;
+    public GameObject quitmenu;
 
     private float velocityFloat;
     private float hVelocityFloat;
     private float vVelocityFloat;
 
-    public GameObject slowdownEffect;
+    private GameObject player;
+    public GameObject ReloadIndicator;
+    public GameObject CaseShellL;
+    public GameObject CaseShellR;
+
+    public GameObject indicator1;
+    public GameObject indicator2;
+    public GameObject indicator3;
+
+    public GameObject winPanel;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
         visisble = false;
         if (hideToggle != null)
         {
             hideToggle.SetActive(visisble);
         }
+        if (winPanel != null)
+        {
+            winPanel.SetActive(false);
+        }
     }
 
+    private bool exitactive = false;
     // Update is called once per frame
     void Update()
     {
-        
+        ReloadIndicator.transform.position = player.transform.position;
+        /*
         if (hideToggle != null && Input.GetButtonDown("Enable Debug Button 1"))
         {
             visisble = !visisble;
             hideToggle.SetActive(visisble);
         }
-
-        if (slowdownEffect != null && Time.timeScale < 1)
-        {
-            slowdownEffect.SetActive(true);
-        }
-        else if (slowdownEffect != null && Time.timeScale >= 1)
-        {
-            slowdownEffect.SetActive(false);
-        }
-
+        */
         
         setText();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            exitactive = !exitactive;
+            quitmenu.SetActive(exitactive);
+            
+            if (exitactive)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+
+        }
+
+        switch(ammoCountInt)
+        {
+            case 0:
+                CaseShellL.SetActive(false);
+                CaseShellR.SetActive(false);
+                break;
+            case 1:
+                CaseShellL.SetActive(false);
+                CaseShellR.SetActive(true);
+                break;
+            case 2:
+                CaseShellL.SetActive(true);
+                CaseShellR.SetActive(true);
+                break;
+        }
         
     }
 
@@ -79,5 +116,23 @@ public class uiController : MonoBehaviour
         velocityFloat = v;
         hVelocityFloat = hV;
         vVelocityFloat = vV;
+    }
+
+    public void updateChargeValues(bool ind1, bool ind2, bool ind3)
+    {
+        indicator1.SetActive(ind1);
+        indicator2.SetActive(ind1);
+        indicator3.SetActive(ind1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CloseMenu()
+    {
+        exitactive = !exitactive;
+        quitmenu.SetActive(exitactive);
     }
 }
