78894622ad263283923efe90aa79aefa3b63ca1b
netcavy <102557854@student.swin.edu.au>
2023-08-09 15:12:02 +1000

    destructible tile radius from blastvalue event

--- Ballistite Project/Assets/Scripts/DestructibleTiles.cs
+++ Ballistite Project/Assets/Scripts/DestructibleTiles.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class DestructibleTiles : MonoBehaviour
 {
     private Transform projectilePos;
+    private float radiusModifier;
+    public float radius;
+    public float radiusMax;
+
+    public void CalcRadius(GameEventData eventData)
+    {
+        if (eventData is PlayerEventData playerData)
+        {
+            radiusModifier = playerData.BlastValue;
+        }
+    }
+
     public void DestroyTiles(GameEventData eventData)
     {
         if (eventData is ProjectileEventData projectileData)
         {
             projectilePos = projectileData.HitPosition; // gets position of event's projectile
 
-            //TODO: check radius around projectile
+            //TODO: check radius around projectile.
+            radius = radius + radiusModifier; // Blast amount adds to radius
+            if (radius >= radiusMax) radius = radiusMax;
 
-            Debug.Log("Projectile Hit Terrain at " + projectilePos.position);
+            Debug.Log("Projectile Hit Terrain at " + projectilePos.position + " and radius will be " + radius);
         }
     }
 }
